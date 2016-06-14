3436704c3e2db5164eed2069300a9a9a2640128a
sethvincent <sethvincent@gmail.com>
2016-06-14 15:16:40 -0700

    no newline at top of sidebar style

--- components/sidebar.js
+++ components/sidebar.js
@@ -4,19 +4,16 @@ var el = require('bel')
 module.exports = function (params, state, send) {
   var contents = state.contents
 
-  var prefix = css`
-    :host {
-      width: 24%;
-      padding-left: 3%;
-      display: inline-block;
-      padding-bottom: 3%;
-      overflow-y: scroll;
-      background: rgb(240,240,240);
-      height: 100%;
-    }
-  `
-  console.log('prefix', prefix)
-  
+  var prefix = css`:host {
+    width: 24%;
+    padding-left: 3%;
+    display: inline-block;
+    padding-bottom: 3%;
+    overflow-y: scroll;
+    background: rgb(240,240,240);
+    height: 100%;
+  }`
+
   function createMenu (contents) {
     return contents.map(function (item) {
       if (item.link) {
