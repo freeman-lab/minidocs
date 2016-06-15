5338ccbf382986e471aa7cd97d6a8ca5164a7617
sethvincent <sethvincent@gmail.com>
2016-06-15 16:46:17 -0700

    source link

--- components/content.js
+++ components/content.js
@@ -5,12 +5,32 @@ module.exports = function (params, state, send) {
   var html = state.html
   var currentPage = params.page || state.current
   var page = html[currentPage]
+  var pageData = state.contents.filter(function (item) {
+    return item.key === currentPage
+  })[0]
 
   var prefix = css`
-    :host {}
+    a.markdown-link {
+      color: #aaa;
+      border: 1px solid #eee;
+      display: inline-block;
+      padding: 3px 5px;
+      text-decoration: none;
+    }
+
+    a.markdown-link:hover {
+      color: #4078c0;
+      border: 1px solid #4078c0;
+    }
   `
 
-  var element = el`<div class="${prefix} minidocs-content"></div>`
-  element.innerHTML = page
-  return element
+  var contentWrapper = el`<div></div>`
+  contentWrapper.innerHTML = page
+  console.log('pagedata.source?', pageData)
+  var link = pageData.source ? el`<a class="markdown-link" href="${pageData.source}">source</a>` : ''
+
+  return el`<div class="${prefix} minidocs-content">
+    ${link}
+    ${contentWrapper}
+  </div>`
 }
