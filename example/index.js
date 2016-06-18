ecbcc88bf9f1736d7be14d4c1134df4944b4394c
freeman <the.freeman.lab@gmail.com>
2016-06-18 00:21:22 -0400

    fix example

--- example/index.js
+++ example/index.js
@@ -1,8 +1,11 @@
 var contents = require('./contents')
 var read = require('read-directory')
 
-require('../index.js')({
+var app = require('../index.js')({
   contents: contents,
   markdown: read.sync('./markdown', { extensions: false }),
   logo: './logo.svg'
 })
+
+var tree = app.start()
+document.body.appendChild(tree)
