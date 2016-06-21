9324a59ad92fc07a107c29e2f586009dbe957d55
sethvincent <sethvincent@gmail.com>
2016-06-21 07:02:27 -0700

    extraneous markdown should not be fatal - closes #27

--- app.js
+++ app.js
@@ -1,24 +1,21 @@
 var choo = require('choo')
 
-var parseContents = require('./lib/parse-contents')
-var parseMarkdown = require('./lib/parse-markdown')
+var parseDocs = require('./lib/parse-docs')
 var main = require('./components/main')
 
 module.exports = function (opts) {
   var app = choo()
-
-  var contents = parseContents(opts.contents)
-  var documents = parseMarkdown(opts.markdown)
+  var docs = parseDocs(opts)
 
   app.model({
     state: {
       title: opts.title,
       logo: opts.logo,
-      contents: contents,
-      markdown: documents.markdown,
-      html: documents.html,
-      routes: documents.routes,
-      current: opts.initial || Object.keys(documents.markdown)[0]
+      contents: docs.contents,
+      markdown: docs.markdown,
+      html: docs.html,
+      routes: docs.routes,
+      current: opts.initial || Object.keys(docs.markdown)[0]
     },
     reducers: {}
   })
