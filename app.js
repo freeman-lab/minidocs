14d93cb2cfcaf2796eb88e4c207e110a1a8ebf84
sethvincent <sethvincent@gmail.com>
2016-06-29 15:06:25 -0700

    improve how intial page is chosen

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ module.exports = function (opts) {
       markdown: docs.markdown,
       html: docs.html,
       routes: docs.routes,
-      current: opts.initial || Object.keys(docs.markdown)[0]
+      current: docs.initial
     },
     reducers: {}
   })
