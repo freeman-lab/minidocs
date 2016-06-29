58f620f6c8203219d181d9fd8555e8840d14de21
sethvincent <sethvincent@gmail.com>
2016-06-29 14:49:04 -0700

    generate routes & md based only on contents

--- cli.js
+++ cli.js
@@ -63,7 +63,7 @@ var state = {
   logo: logo,
   contents: require(contentsPath),
   markdown: markdown,
-  initial: argv.initial || Object.keys(markdown)[0]
+  initial: argv.initial
 }
 
 var docs = parseDocs(state)
