130d1619a335ac25323be84910ead076d000a483
sethvincent <sethvincent@gmail.com>
2016-07-06 10:52:40 -0700

    tests dir

---
+++ tests/app.js
@@ -0,0 +1,37 @@
+var test = require('tape')
+var xtend = require('xtend')
+
+var createApp = require('../app')
+var parseDocs = require('../lib/parse-docs')
+
+test('app.js returns choo app', function (t) {
+  var options = require('./fixtures/app-options')
+  var app = createApp(options)
+  t.ok(app)
+  t.ok(app.start)
+  t.ok(app.toString)
+  t.end()
+})
+
+if (process.env.TEST_ENV !== 'node') {
+  test('app.start() returns dom tree', function (t) {
+    var options = require('./fixtures/app-options')
+    var app = createApp(options)
+    var tree = app.start()
+    t.ok(tree)
+    t.end()
+  })
+}
+
+test('app.toString() returns html', function (t) {
+  var options = require('./fixtures/app-options')
+  var app = createApp(options)
+  var docs = parseDocs(options)
+  options.html = docs.html
+  options.contents = docs.contents
+  var html = app.toString('/', options)
+  t.ok(html)
+  t.end()
+})
+
+
---
+++ tests/index.js
@@ -0,0 +1,2 @@
+require('./parse-docs')
+require('./app')
